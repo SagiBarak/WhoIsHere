872da9ab1b159a9d70f0775389d6dbab322d8f5f
sagib <sagi1506@gmail.com>
2017-11-17 15:58:48 +0200

    Initial commit

---
+++ app/src/main/java/com/sagib/whoishere/MainActivity.java
@@ -0,0 +1,92 @@
+package com.sagib.whoishere;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    @BindView(R.id.frame)
+    FrameLayout frame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        checkForUser();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                updateUI();
+                return;
+            } else {
+                checkForUser();
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showToast("Sign in Cancelled");
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showToast("No Internet Connection");
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showToast("Unknown Error");
+                    return;
+                }
+            }
+            showToast("Unknown Response");
+        }
+    }
+
+    private void checkForUser() {
+        if (auth.getCurrentUser() != null) {
+            updateUI();
+        } else {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(
+                                    Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    private void updateUI() {
+        Toast.makeText(this, "Updating UI...", Toast.LENGTH_SHORT).show();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.frame, new Fragment(), "HomeFragment").commit();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
